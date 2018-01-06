2b34d9f6374dbbe23eb95356321652d3a7f83573
Wilco van Esch <wilco.vanesch@gmail.com>
2018-01-06 23:33:42 +0000

    Update filename not allowed in Windows.

--- test/weird.js
+++ test/weird.js
@@ -12,12 +12,14 @@ server.listen(port, host, started);
 function started() {
   console.log('server started');
 
-  var file = encodeURIComponent('weird filename$?..\nnewline');
-  http.request('http://localhost:9128/test/' + file, function(res) {
-    console.log('GET /' + file);
-    console.log('-> statusCode = %d', res.statusCode);
-    console.dir(res.headers);
-    assert(res.statusCode === 200);
-    process.exit(0);
-  }).end();
+  var file = encodeURIComponent('weird filename$..\nnewline');
+  http
+    .request('http://localhost:9128/test/' + file, function(res) {
+      console.log('GET /' + file);
+      console.log('-> statusCode = %d', res.statusCode);
+      console.dir(res.headers);
+      assert(res.statusCode === 200);
+      process.exit(0);
+    })
+    .end();
 }
